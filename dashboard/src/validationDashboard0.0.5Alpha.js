2d7eb61f6dee6d9383a1c5c67404789d623fd48c
donthebike <kobosch42@gmail.com>
2019-02-18 00:15:51 +0100

    many improvements, cell selection in table

--- dashboard/src/validationDashboard0.0.5Alpha.js
+++ dashboard/src/validationDashboard0.0.5Alpha.js
@@ -10,18 +10,19 @@
 const mergeOptions = require('merge-options')
 
 let _defaultOptions = {}
-let colorScaleValues = d3.scaleOrdinal().domain(["0", "1", "NA"]).range(["#FC8D59", "#91CF60", "#FFFFBF"])
+let colorScaleValues = d3.scaleOrdinal().domain(["0", "1", "NA"]).range(["#FC8D59", "#91CF60", "#FFFFBF"]) // 0 = fail, 1 = pass, NA = NA
 let colorScaleOpacityPasses = d3.scaleOrdinal().domain([1, 10]).range([0.4, 1]) // domain is set dynamically
 let colorScaleOpacityFails = d3.scaleOrdinal().domain([1, 10]).range([0.4, 1]) // domain is set dynamically
 let colorScaleOpacityNAs = d3.scaleOrdinal().domain([1, 10]).range([0.4, 1]) // domain is set dynamically
 let myndx, valueDim, severityDim, agentDim, valueCount, severityCount, actorDim, actorCount, all
 let ndx
 let datatable
+let datatableShowAllRows = true
+let datatableColoredCells = new Map() // For administering metadata for each colored cell
 
 class validationDashboard {
 
 	constructor(options) {
-		//console.log(options, _defaultOptions)
 		
 		this.options = mergeOptions(_defaultOptions, options)
 		if ("container" in this.options == false) {console.log('container not set'); return; }
@@ -33,8 +34,7 @@ class validationDashboard {
 		// dc needs this:
 		dc.config.defaultColors(d3.schemeAccent)
 		
-		// Create divs:
-
+		// Create divs and headers:
 		var row = d3.select(this.options.container).append("div").classed("row", true)
 		var left = row.append("div").classed("col-4", true)
 		var right = row.append("div").classed("col-8", true)
@@ -43,17 +43,15 @@ class validationDashboard {
 			.text("selected out of <span class='total-count'></span> records.")
 		function addDiv(container, id, label) {
 			var el = container.append("div").attr("id", id).append("div")
-			el.append("strong").text(label)
+			el.append("strong").classed("selectorHeader", true).text(label)
 			el.append("a").classed("reset", true).style("visibility", "hidden").style("margin-left", "5px").attr("href", "#").text("reset")
 			return(el)
 		}
-		addDiv(left, "divChartValue", "Events by value")
-		addDiv(left, "divChartSeverity", "Events by severity")
-		//addDiv(left, "divChartActor", "Events by actor")
-		//addDiv(left, "divChartAgent", "Events by agent")
-		addDiv(left, "divChartRule", "Events by rule")
+		addDiv(left, "divChartValue", "Results")
+		addDiv(left, "divChartSeverity", "Severity")
+		addDiv(left, "divChartRule", "Rules")
 		
-		// Experimental data table:
+		// Data table:
 		let columns = []
 		function escapeDots(s) {return(s.replace(/\./g, "\\."))}
 		for (col of Object.keys(this.options.data[0])) {
@@ -67,29 +65,94 @@ class validationDashboard {
 			scrollY: "600px",
 			scrollCollapse: true,
 			paging: false,
-			rowId: this.options.idcol
+			rowId: this.options.idcol,
+			info: false,
+			select: {
+				style: 'single',
+				items: 'cell',
+				info: false
+			},
+			dom: 'Bfrtip',
+			stateSave: true,
+			buttons: [
+				{
+					extend: 'collection',
+					autoClose: true,
+					text: 'Row visibility',
+					buttons: [{
+							text: 'All rows',
+							action: ( e, dt, node, config ) => {
+								datatableShowAllRows = true
+								this._updateTable()
+							}
+						}, {
+							text: 'Colored rows',
+							action: ( e, dt, node, config ) => {
+							//action: function ( e, dt, node, config ) {
+								datatableShowAllRows = false
+								this._updateTable()
+							}
+						}
+					]
+				},
+				'colvis'
+			]
 		})
+		datatable.on( 'mouseenter', 'tbody td', function () {
+			if ($(this).hasClass('selected') == false) {
+				$(this).css("padding", 3)
+				$(this).css("border", "1px solid #888")
+			}
+		})
+		datatable.on( 'mouseleave', 'tbody td', function () {
+			if ($(this).hasClass('selected') == false) {
+				cellNormalState($(this))
+			}
+		})
+		datatable.on( 'select', function ( e, dt, type, indexes ) {
+			var cell = datatable.cell( indexes )
+			$( cell.node() ).css("padding", 1)
+			$( cell.node() ).css("border", "3px dashed black")
+			
+			// Filter ndx on id dimension to all events where this cell is in the target:
+			let ids = []
+			if (cell.node().cellId) {
+				let events = datatableColoredCells.get(cell.node().cellId).events
+				for (const e of events) {
+					ids.push(e.id)
+					// TODO: do something for other values
+					//if (e.value == "0") ids.push(e.id)
+				}
+			}
+			idDim.filterFunction( function(id) {
+				return (ids.indexOf(id) != -1)
+			})
+		})
+		datatable.on( 'deselect', function ( e, dt, type, indexes ) {
+			var cell = datatable.cell( indexes )
+			cellNormalState($( cell.node() ))
+			
+			idDim.filterFunction( function(id) {
+				return (true)
+			})
+		})
+		function cellNormalState(cell) {
+			cell.css("padding", 4)
+			cell.css("border-top", "1px solid #ddd")
+			cell.css("border-right", "1px solid #ddd")
+			cell.css("border-bottom", "")
+			cell.css("border-left", "")
+		}
 		
-		// End experimental.....
-		
-		// event table
-		/*d3.select(this.options.container).append("div").attr("id", "divTableEvents")*/
-
 		// Create dc charts:
 		this.eventCount = dc.dataCount('#divEventCount')
-		this.chartValue = dc.pieChart("#divChartValue")
+		this.chartValue = dc.rowChart("#divChartValue")
 		this.chartSeverity = dc.rowChart("#divChartSeverity")
-		//this.chartActor = dc.rowChart("#divChartActor")
-		//this.chartAgent = dc.rowChart("#divChartAgent")
-		//this.chartRule = dc.barChart('#divChartRule')
 		this.chartRule = dc.rowChart('#divChartRule')
-		//this.tableEvents = dc.dataTable('#divTableEvents')
 
 		// Add callbacks for reset buttons:
 		d3.select("#divChartValue a").on("click", () => this.chartValue.filter(null).redrawGroup() )
 		d3.select("#divChartSeverity a").on("click", () => this.chartSeverity.filter(null).redrawGroup() )
-		//d3.select("#divChartActor a").on("click", () => this.chartActor.filter(null).redrawGroup() )
-		//d3.select("#divChartAgent a").on("click", () => this.chartAgent.filter(null).redrawGroup() )
 		d3.select("#divChartRule a").on("click", () => this.chartRule.filter(null).redrawGroup() )
 
 		// set crossfilter dimensions and counts:
@@ -100,7 +163,6 @@ class validationDashboard {
 		severityDim = ndx.dimension(d => d.rule.severity)
 		agentDim = ndx.dimension(d => d.event.agent)
 		actorDim = ndx.dimension(d => d.event.actor)
-		//ruleDim = ndx.dimension(d => "id" in d.rule ? d.rule.id : this._expressionShort(d.rule.expression))
 		ruleDim = ndx.dimension(d => "id" in d.rule ? d.rule.id : this._expressionShort(d.rule.source))
 		
 		valueCount = valueDim.group().reduceCount()
@@ -111,33 +173,41 @@ class validationDashboard {
 		all = ndx.groupAll()
 		
 		// Events count:
+		window.resetAll = function() {
+			dc.filterAll()
+			dc.renderAll()
+			datatable.cells( { selected: true } ).deselect()
+		}
 		this.eventCount
 		.dimension(ndx)
 		.group(all)
 		.html({
-			some: '<strong>%filter-count</strong> selected out of <strong>%total-count</strong> events' +
-			' | <a href=\'javascript:dc.filterAll(); dc.renderAll();\'>Reset All</a>',
-			//all: 'All events selected. Click on the graphs to apply filters.'
-			all: '<strong>%filter-count</strong> selected out of <strong>%total-count</strong> events'
-		});
+			some: '<strong>%filter-count</strong> out of <strong>%total-count</strong> selected' +
+			' | <a href=\'javascript:resetAll();\'>Reset All</a>',
+			all: '<strong>%filter-count</strong> out of <strong>%total-count</strong> selected'
+		})
 
 		// Events by Value:
 		this.chartValue
-		.width(200).height(100)
+		.width(200).height(120)
 		.dimension(valueDim)
 		.group(valueCount)
 		.controlsUseVisibility(true)
-		.innerRadius(0)
+		.elasticX(true)
+		//.innerRadius(0)
 		.label(d => d.key == 0 ? "fails" : (d.key == 1 ? "passes" : "NA"))
 		.title(d => d.key == 0 ? "fails: "+d.value : (d.key == 1 ? "passes: "+d.value : "NA: "+d.value))
 		.colors(d => colorScaleValues(d))
 		//.externalLabels(10)
 		//.externalRadiusPadding(10)
 		//.drawPaths(true)
+		//.replaceFilter([["0"]])		// Init selection to fails:
+		.margins({top: 0, right: 10, bottom: 30, left: 5})
+
 
 		// Events by severity:
 		this.chartSeverity
-		.width(200).height(100)
+		.width(200).height(120)
 		.dimension(severityDim)
 		.group(severityCount)
 		.controlsUseVisibility(true)
@@ -153,131 +223,61 @@ class validationDashboard {
 			}
 			return label;
 		})
-		.legend(dc.legend());
-
-		// Events by actor:
-		/*this.chartActor
-		.width(250).height(200)
-		.dimension(actorDim)
-		.group(actorCount)
-		.controlsUseVisibility(true)
-		.elasticX(true)
-		.ordinalColors(['#3182bd'])
-		.renderTitleLabel(false);
-
-		// Events by agent:
-		this.chartAgent
-		.width(250).height(200)
-		.dimension(agentDim)
-		.group(agentCount)
-		.controlsUseVisibility(true)
-		.ordinalColors(['#3182bd'])
-		.elasticX(true);*/
+		.legend(dc.legend())
+		.margins({top: 0, right: 10, bottom: 30, left: 5})
 		
 		// Events by rule:
 		this.chartRule
-		.width(400).height(500)
+		.width(400).height(450)
 		.dimension(ruleDim)
 		.group(ruleCount)
 		.controlsUseVisibility(true)
 		.ordinalColors(['#3182bd'])
-		.elasticX(false)
+		.elasticX(true)
 		.ordering(d => d.key ) // sort on rule alphabetically instead of value
 		//.y(d3.scaleBand())
+		.margins({top: 0, right: 50, bottom: 30, left: 5})
 		
-		/*this.chartRule
-		.width(800)
-		.height(300)
-		.x(d3.scaleBand())
-		.xUnits(dc.units.ordinal)
-		.brushOn(true)
-		.xAxisLabel('Rules')
-		.yAxisLabel('Count')
-		.dimension(ruleDim)
-		.barPadding(0.1)
-		.outerPadding(0.05)
-		.group(ruleCount)
-		.title( d => d.key + "\n" + d.value)
-		.controlsUseVisibility(true)
-		.elasticY(true)
-		.ordinalColors(['#3182bd'])
-		.xAxis().tickFormat("");*/
-
-
-
-		// Events grid:
-		/*this.tableEvents
-		.dimension(idDim)
-		.group(d => d.id)
-		.showGroups(false)
-		.columns([
-				'id', 'value', {
-					label: 'time',
-					format: d => d.event.time
-				}, {
-					label: 'severity',
-					format: d => d.rule.severity == "error" ? "E" : (d.rule.severity == "information" ? "I" : "W")
-				}, {
-					label: 'language',
-					format: d => d.rule.language
-				}, {
-					label: 'change',
-					format: d => d.rule.change
-				}, {
-					label: 'actor',
-					format: d => d.event.actor
-				}, {
-					label: 'agent',
-					format: d => d.event.agent
-				}, {
-					label: 'trigger',
-					format: d => d.event.trigger
-				}
-			])
-		.on("renderlet", function (chart) {
-			chart.selectAll('tr.dc-table-row').style('background-color', function (d) {
-				return colorScaleValues(d.value)
-			})
-		})*/
-
 		dc.renderAll();
 		
-		ndx.onChange(eventType => {if (eventType == "filtered") this._showFails()} )
-		this._showFails()
+		ndx.onChange(eventType => {
+			if (eventType == "filtered") {
+				this._updateTable()
+				// TODO: update charts and count
+				dc.redrawAll();
+			}
+		})
+		this._updateTable()
 		
 		return(this)
 	}
-	
-	/*_buildRuleMap(this.options.report) {
-		for (var i=0; i<this.options.report.length; i++) {
-			var rule = this.options.report[i].rule;
-			var exprShort = this._expressionShort(rule.expression)
-			if (exprShort in ruleMap) continue;
-			ruleMap[exprShort] = rule.description;
-		}
-		return(ruleMap)
-	}*/
 
 	_expressionShort(expression) {
 		return (expression.substr(0, 160))
 	}
 	
-	_showFails() {
-		// Clean all cells and set count to 0:
-		$( datatable.cells().nodes() ).css("background-color", '')
+	_updateTable() {
+		let coloredRows = this._setBackgroundColors()
+		this._updateRowVisibility(coloredRows)
+	}
+	
+	_setBackgroundColors() {
 		
-		// Map for storing cells info:
-		let cells = new Map()
+		let coloredRows = new Set()
+		
+		// Clean all cells:
+		$( datatable.cells().nodes() ).css("background-color", '')
+		datatableColoredCells.clear()
 		
 		// Calculate cell statistics and calculate maximum error cnt:
 		let maxCnts = new Map([["0", 0],["1", 0],["NA", 0]])
 		for (const e of ndx.allFiltered()) {
 			for (const t of e.data.target) {
 				let cellId = t[2]+t[3]
-				let item = cells.get(cellId)
+				let item = datatableColoredCells.get(cellId)
 				if (!item) {
-					cells.set(cellId, {row: t[2], col: t[3], cnts: new Map([["0", 0],["1", 0],["NA", 0]]), events: []})
-					item = cells.get(cellId)
+					datatableColoredCells.set(cellId, {row: t[2], col: t[3], cnts: new Map([["0", 0],["1", 0],["NA", 0]]), events: []})
+					item = datatableColoredCells.get(cellId)
 				}
 				item.cnts.set(e.value, item.cnts.get(e.value)+1)
 				item.events.push(e)
@@ -289,15 +289,18 @@ class validationDashboard {
 		
 		
 		// Set domain of colorScaleOpacityCells:
-		console.log(maxCnts)
 		colorScaleOpacityPasses.domain([1, maxCnts.get("1")])
 		colorScaleOpacityFails.domain([1, maxCnts.get("0")])
 		colorScaleOpacityNAs.domain([1, maxCnts.get("NA")])
 		
-		for (var [cellId, item] of cells) {
+		for (var [cellId, item] of datatableColoredCells) {
 			let rowSelector = "#"+item.row
 			let colSelector = item.col+':name'
 			let cell = datatable.cell(rowSelector, colSelector)
+			if (cell.node()) cell.node().cellId = cellId		// Register cellId for use in mouseover
+			
+			// register colored rows:
+			coloredRows.add(rowSelector)
 			
 			// Determine background color:
 			let failDetected = false
@@ -310,14 +313,24 @@ class validationDashboard {
 			
 			// Set background-color:
 			let bgcolor = d3.color(colorScaleValues(value))
-			let cnt = item.cnts.get(value)
-			bgcolor.opacity = (value == "1") ? colorScaleOpacityPasses(cnt) : ( (value == "0") ? colorScaleOpacityFails(cnt) : colorScaleOpacityNAs(cnt) )
+			if (value == "0") {
+				// Calculate opacity based on maxCnt:
+				bgcolor.opacity = colorScaleOpacityFails(item.cnts.get(value))
+			}
 			$( cell.node() ).css("background-color", bgcolor.toString())
-			
-			console.log(cellId, value, cnt)
-			
 		}
 		
+		return(coloredRows)
+	}
+	
+	_updateRowVisibility(coloredRows) {
+		if (datatableShowAllRows) {
+			$( datatable.rows().nodes() ).css("display", "table-row")
+		}
+		else {
+			$( datatable.rows().nodes() ).css("display", "none")
+			for (const rowSelector of coloredRows) $( datatable.row(rowSelector).node() ).css("display", "table-row")
+		}
 	}
 	
 }
